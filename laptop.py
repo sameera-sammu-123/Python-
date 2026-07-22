e63f26f3a8fae602a0a33512273b6e3354cfe04e
sameera-sammu-123 <sammerasammu77@gmail.com>
2026-07-22 14:48:55 +0530

    Add files via upload

---
+++ laptop.py
@@ -0,0 +1,20 @@
+product = input("Enter product name")
+quality1 = int(input("Enter  quality 1:"))
+price1 = int(input("Enter a price 1:"))
+quality2 = int(input("Enter quality2:"))
+price2 = int(input("Enter a price2:"))
+amount1 = quality * price1
+amount2 = quality * price2
+total = amount1 + amount2
+if total > 50000:
+   discount - total * 10 / 100
+else:
+   discount = 0
+final  amount - total - discount
+print("Product Name :", product)
+print("Total Amount :", total)
+print("Discount :", discount)
+print("Final Amount :", final_amount)
+
+
+
